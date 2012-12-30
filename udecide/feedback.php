5fbf51dee35c9a30a16181e94f8f334e8983e1de
Andy Li <andy.li.jj@gmail.com>
2012-12-30 11:34:37 +0800

    unified header in feedback.php

--- udecide/feedback.php
+++ udecide/feedback.php
@@ -14,13 +14,48 @@ require_once 'cFeedback.php';
 <link href="css/bootstrap-responsive.css" rel="stylesheet">
 <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+<style type="text/css">
+.logo {
+	float: left;
+	margin: 25px;
+	display: inline;
+}
+.nav {
+	position: relative;
+	float: right;
+	display: inline;
+	right: 100px;
+}
+.nav li {
+	float:left;
+	margin-right: 15px;
+	margin-left: 15px;
+	font-size: 18px;
+	margin-top: 45px;
+}
+.nav li a {
+	color: #666;
+}
+.nav li a:hover {
+	color: #000;
+	background-color: #EEE;
+}
+</style>
+
 </head>
 
 <body>
-    <div id="head" class="container hidden-tablet hidden-phone" style="background-color: #EEE; margin-bottom: 30px">
+    <div class="container" style="background-color: #EEE; margin-bottom: 30px">
     <!--Side bar-->
     <a href="index.php"><img src="img/udecide_logo6.png" class="logo"/></a>
     <!--Survey menu-->
+    <ul class="nav hidden-tablet hidden-phone">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="examples.php">Examples</a></li>
+            <li><a href="contact.php">Contact us</a></li>
+    </ul>
     </div>
     <div id="result" class="resultnone">
     <h1 style="text-align: center; color: #093">Thank you for your participation!</h1>
