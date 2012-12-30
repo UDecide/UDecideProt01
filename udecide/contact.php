c484b8dbbbcf1097aa41091ed9e9d86e0979afe5
Samuel <dijkstrapass@gmail.com>
2012-12-30 20:09:09 +0800

    add information

--- udecide/contact.php
+++ udecide/contact.php
@@ -1,54 +1,57 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<title>UDecide</title>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>UDecide</title>
 
-<link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
-<link href="css/bootstrap-responsive.css" rel="stylesheet"/>
-<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+        <link href="css/bootstrap-responsive.css" rel="stylesheet"/>
+        <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-<style type="text/css">
-.logo {
-	float: left;
-	margin: 25px;
-	display: inline;
-}
-.nav {
-	position: relative;
-	float: right;
-	display: inline;
-	right: 100px;
-}
-.nav li {
-	float:left;
-	margin-right: 15px;
-	margin-left: 15px;
-	font-size: 18px;
-	margin-top: 45px;
-}
-.nav li a {
-	color: #666;
-}
-.nav li a:hover {
-	color: #000;
-	background-color: #EEE;
-}
-</style>
-</head>
+        <style type="text/css">
+            .logo {
+                float: left;
+                margin: 25px;
+                display: inline;
+            }
+            .nav {
+                position: relative;
+                float: right;
+                display: inline;
+                right: 100px;
+            }
+            .nav li {
+                float:left;
+                margin-right: 15px;
+                margin-left: 15px;
+                font-size: 18px;
+                margin-top: 45px;
+            }
+            .nav li a {
+                color: #666;
+            }
+            .nav li a:hover {
+                color: #000;
+                background-color: #EEE;
+            }
+        </style>
+    </head>
 
-<body>
-    <div class="container" style="background-color: #EEE; margin-bottom: 30px">
-    <!--Side bar-->
-    <a href="index.php"><img src="img/udecide_logo6.png" class="logo"/></a>
-    <!--Survey menu-->
-    <ul class="nav hidden-tablet hidden-phone">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="services.php">Services</a></li>
-            <li><a href="examples.php">Examples</a></li>
-            <li><a href="contact.php">Contact us</a></li>
-    </ul>
-    </div>
-</body>
+    <body>
+        <div class="container" style="background-color: #EEE; margin-bottom: 30px">
+            <!--Side bar-->
+            <a href="index.php"><img src="img/udecide_logo6.png" class="logo"/></a>
+            <!--Survey menu-->
+            <ul class="nav hidden-tablet hidden-phone">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="examples.php">Examples</a></li>
+                <li><a href="contact.php">Contact us</a></li>
+            </ul>
+        </div>
+        <div class="container">
+            <h2>The contact information goes here. </h2>
+        </div>
+    </body>
 </html>
