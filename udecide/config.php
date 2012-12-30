f08902df1c6a1d40e948321dd60fca8b4e4e04cd
Samuel <dijkstrapass@gmail.com>
2012-12-30 13:18:02 +0800

    Modification for cloudcontrol

--- udecide/config.php
+++ udecide/config.php
@@ -1,23 +1,27 @@
 <?php
-//Always provide a TRAILING SLASH (/) AFTER A PATH
-define('URL','http://localhost/zm/mvc/');
-define('URL_VIEWS',URL.'views/');
-define('LIBS','libs/');
 
-define('DB_TYPE','mysql');
-define('DB_HOST','localhost');
-define('DB_NAME','udecide');
-define('DB_USER','root');
-define('DB_PASS','');
+# read the credentials file
+$string = file_get_contents($_ENV['CRED_FILE'], false);
+if ($string == false) {
+   //die('FATAL: Could not read credentials file');
+    define('DB_TYPE', 'mysql');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'udecide');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+}
+
+# the file contains a JSON string, decode it and return an associative array
+$creds = json_decode($string, true);
+
+# now use the $creds array to configure your app e.g.:
+define('DB_TYPE', $MYSQL_HOSTNAME = $creds['MYSQLS']['MYSQLS_DATABASE']);
+define('DB_HOST', $MYSQL_HOSTNAME = $creds['MYSQLS']['MYSQLS_HOSTNAME']);
+define('DB_NAME', $MYSQL_HOSTNAME = 'udecide');
+define('DB_USER', $MYSQL_HOSTNAME = $creds['MYSQLS']['MYSQLS_USERNAME']);
+define('DB_PASS', $MYSQL_HOSTNAME = $creds['MYSQLS']['MYSQLS_PASSWORD']);
 
 
 define('ERROR_LOGIN','The email or the password is not correct @_@');
 define('ERROR_SIGNUP','The email has already been registered >_<');
-
-//The sitewide hashkey, do not change this becasue it is used for passwords!
-//This is for other hash keys..Not sure yet
-define('HASH_GENERAL_KEY','gamimag');
-
-//This is for database passwords only
-define('HASH_PASSWORD_KEY','gg');
 ?>
